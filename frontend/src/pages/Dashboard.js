fe1231980fe8cf683fb8ed4f97758018817e57c4
ops86199 <swainomprakash12@gmail.com>
2025-12-03 01:39:40 +0530

    Update Dashboard.js

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -14,7 +14,7 @@ export default function Dashboard() {
 
     useEffect(() => {
         const fetchMetrics = () => {
-            fetch("http://backend-svc:8081/metrics/summary")
+            fetch("backend/metrics/summary")
                 .then(res => res.json())
                 .then(data => {
                     setCpu(data.cpu);
